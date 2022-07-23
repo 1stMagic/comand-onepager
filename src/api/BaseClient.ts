443bd20a68875edaac1e76546aea44563980119d
biock <biock@biock.com>
2022-07-23 16:30:11 +0200

    Cleanup

--- src/api/BaseClient.ts
+++
@@ -1,7 +0,0 @@
-export abstract class BaseClient {
-    private static readonly BASE_URL = `${process.env.BASE_URL}templates/pages/data`;
-
-    protected getUrl(path: string): string {
-        return `${BaseClient.BASE_URL}${path}`;
-    }
-}
